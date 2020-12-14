0d45c606eeb4f883f23d8d3a5bf888df39b3c60d
Luana França <71330612+Luana-21898@users.noreply.github.com>
2020-12-14 22:42:24 +0000

    Update lab1.js

--- lab1.js
+++ lab1.js
@@ -1,10 +1,10 @@
 var firstName = 'Luana';
 var favouriteColor = 'Purple';
-var favouriteFood = 'Pizza!';
-var city = 'Dublin';
+var favouriteFood = 'Pizza';
+var city = 'Rio de Janeiro.';
 
-//This line of code display the value of each variable;
-console.log(firstName + ' favorite color is: ' + favouriteColor + ' favorite food is: ' + favouriteFood + ' City is: ' + city);
+//The line below will display the value for the variables firstName, favouriteColor, favouriteFood and city.
+console.log(firstName + 's' + ' favorite color is: ' + favouriteColor + ', favorite food is: ' + favouriteFood + ' and her city is: ' + city);
 
 var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
 var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
