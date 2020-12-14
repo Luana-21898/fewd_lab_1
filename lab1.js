b459a0d43898ce45e0cbeeac3b5a5199e934c429
Luana-21898 <21898@student.dorset-college.ie>
2020-12-14 20:39:51 +0000

    commit

---
+++ lab1.js
@@ -0,0 +1,19 @@
+var firstName = 'Luana';
+var favouriteColor = 'Purple';
+var favouriteFood = 'Pizza!';
+var city = 'Dublin';
+
+//This line of code display the value of each variable;
+console.log(firstName + ' favorite color is: ' + favouriteColor + ' favorite food is: ' + favouriteFood + ' City is: ' + city);
+
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+console.log(phrase + ' ' + one + ', ' + two + ', ' + three + ', ' + four + ', ' + five + ', ' + six + ', ' + seven + ' ' + endingPhrase);
